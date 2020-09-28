daf14571cf2653f30311e6eb5d451dfcd1284a23
Pratheesh Prakash <pratheeshraniprakash@gmail.com>
2020-09-28 09:40:12 +0530

    Delete TwitterConf.py

--- TwitterConf.py
+++
@@ -1,8 +0,0 @@
-#Consumer key/secret, access token/secret maybe obtained from https://dev.twitter.com/
-
-consumer_key = 'asdasdasdasdasdasdasdasdasdasd'
-consumer_secret = 'sdfsdfasdfsdfsdfsdfsdfsdfsdfsdf'
-access_token = 'sdfsadfsadfasdfasdfsdfsdfsdfsdfsd'
-access_secret = 'sdfasdfdsfdsfsdfsdfsdfsdfsdfsdf'
-
-#Currently using the credentials of @NewsClickin - Sputnik V2.0
